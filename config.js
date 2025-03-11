d781f85a6115c5ab378ebc3b001cf6d8f0f657b5
udhaindah <udhaindah937@gmail.com>
2025-03-12 05:49:01 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    targetUserIds: [
+        "2497966"
+    ], // Target user ID for points transfer & follow
+    telegramBotToken: "bottoken", // Telegram bot token
+    telegramChatId: "idtele",   // Telegram chat ID
+    maxConcurrentProcesses: 5 // Maximum number of concurrent processes
+};
